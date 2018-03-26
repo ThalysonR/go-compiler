378d85196a1f08b491f8f3f8c953ba180078cf56
Thalyson Rocha <tcastro@alfa.corp>
2018-03-26 13:49:37 -0400

    Keyword

--- main.go
+++ main.go
@@ -15,61 +15,101 @@ const (
 	LiteralToken
 
 )
-
+var Keywords  = [25]string {
+	"break", "case", "chan", "const", "continue",
+	"default", "defer", "else", "fallthrough", "for",
+	"func", "go", "goto", "if", "import",
+	"interface", "map", "package", "range", "return",
+	"select", "struct", "switch", "type", "var",
+}
 
 func main()  {
-	teste := .25
+	teste := "case umdoistres"
+
+	l := lexer.New(teste, Root)
+
+	l.Start()
+
+	for tok, done := l.NextToken(); done; {
+		fmt.Printf("%q - %q", tok.Value, tok.Type)
+	}
+	//for r := l.Next(); r != lexer.EOFRune ; {
+	//	fmt.Println(string(r))
+	//	r = l.Next()
+	//}
 
 }
 
 func Root(l *lexer.L) lexer.StateFunc {
-	for unicode.IsSpace(l.Peek()) {
+	//r := l.Next()
+	fmt.Print("Passa 0")
+	for unicode.IsSpace(l.Next()) {
 		l.Ignore()
 	}
+	l.Rewind()
+
+	r := l.Peek()
+	fmt.Print("Passa 1")
+	fmt.Println("r : " + string(r))
 	if unicode.IsLetter(l.Peek()) {
 		return CheckKeyword(l)
 	}
+	fmt.Print("Passa 2")
 
-	r := l.Next()
-	 if r == '_' {
+
+	if r == '_' {
 		return Identifier(l)
-	} else if unicode.IsSymbol(r) || unicode.IsPunct(r) {
-		return OpAndPunc(l)
-	} else if unicode.IsDigit(r) || r == '.'  {
-		return Numbers(l)
-	} else if r == '\'' {
-		return Runes(l)
-	} else if r == '"' {
-		return Strings(l)
+		//} else if unicode.IsSymbol(r) || unicode.IsPunct(r) {
+		//	return OpAndPunc(l)
+		//} else if unicode.IsDigit(r) || r == '.'  {
+		//	return Numbers(l)
+		//} else if r == '\'' {
+		//	return Runes(l)
+		//} else if r == '"' {
+		//	return Strings(l)
 	} else {
-		l.Error(fmt.Sprintf("Símboolo desconhecido %q", r))
+		l.Error(fmt.Sprintf("Símbolo desconhecido %q", r))
 	}
 
 	return nil
 }
 
 func CheckKeyword(l *lexer.L) lexer.StateFunc {
-	var Keywords  = [25]string {
-		"break", "case", "chan", "const", "continue",
-		"default", "defer", "else", "fallthrough", "for",
-		"func", "go", "goto", "if", "import",
-		"interface", "map", "package", "range", "return",
-		"select", "struct", "switch", "type", "var",
-	}
 	r := l.Next()
-	for i, word := range Keywords {
-		for j, c := range word {
-			if r != c {
+	fmt.Println("r: " + string(r))
+	kw := Keywords
+	counter := 0
+	for _, word := range kw {
+		for _, lt := range word {
+			fmt.Printf("%q = %q\n", string(r), string(lt))
+			if lt != r {
 				break
 			} else {
+				counter++
 				r = l.Next()
 			}
-			if l.Current() == word && unicode.IsSpace(l.Peek()) {
-				l.Emit(KeywordToken)
-				return Root(l)
-			} else {
-
+		}
+		if unicode.IsSpace(r) {
+			fmt.Println("Match: " + word)
+			for  ;counter >= 0; counter-- {
+				l.Rewind()
 			}
+			r = l.Next()
 		}
 	}
+
+	return Identifier(l)
+}
+
+func Identifier(l *lexer.L) lexer.StateFunc {
+	r := l.Next()
+
+	for unicode.IsLetter(r) || unicode.IsDigit(r) {
+		fmt.Println("ID: " + string(r))
+		r = l.Next()
+	}
+	l.Rewind()
+
+	l.Emit(IdentifierToken)
+	return Root(l)
 }
