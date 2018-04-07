edcc4f8e7c976c114b8b1e25f437a1c009256f56
Thalyson <thalysonr.castro@hotmail.com>
2018-04-07 10:34:12 -0400

    Deu ruim no string

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"github.com/bbuck/go-lexer"
 	"fmt"
-	"unicode"
+	"github.com/bbuck/go-lexer"
 	"strings"
+	"unicode"
+	"os"
+	"io/ioutil"
 )
 
 const (
-	_  = iota
+	_ = iota
 	IdentifierToken
 	KeywordToken
 	OpAndPuncToken
@@ -17,22 +19,22 @@ const (
 	ImagLiteralToken
 	RuneLiteralToken
 	StringLiteralToken
-
 )
-var Keywords  = [25]string {
+
+var Keywords = [25]string{
 	"break", "case", "chan", "const", "continue",
 	"default", "defer", "else", "fallthrough", "for",
 	"func", "go", "goto", "if", "import",
 	"interface", "map", "package", "range", "return",
 	"select", "struct", "switch", "type", "var",
 }
-var OpAndPuncs = [47]string {
-	"+",    "&",     "+=",    "&=",     "&&",    "==",    "!=",    "(",    ")",
-	"-",    "|",     "-=",    "|=",     "||",    "<",     "<=",    "[",    "]",
-	"*",    "^",     "*=",    "^=",     "<-",    ">",     ">=",    "{",    "}",
-	"/",    "<<",    "/=",    "<<=",    "++",    "=",     ":=",    ",",    ";",
-    "%",    ">>",    "%=",    ">>=",    "--",    "!",     "...",   ".",    ":",
-	"&^",          "&^=",
+var OpAndPuncs = [47]string{
+	"+", "&", "+=", "&=", "&&", "==", "!=", "(", ")",
+	"-", "|", "-=", "|=", "||", "<", "<=", "[", "]",
+	"*", "^", "*=", "^=", "<-", ">", ">=", "{", "}",
+	"/", "<<", "/=", "<<=", "++", "=", ":=", ",", ";",
+	"%", ">>", "%=", ">>=", "--", "!", "...", ".", ":",
+	"&^", "&^=",
 }
 
 func isHex(r rune) bool {
@@ -43,21 +45,22 @@ func isOctal(r rune) bool {
 	return strings.ContainsRune("01234567", r)
 }
 
-func main()  {
-	teste_1 := "    case && switcheroo .10254512i '\U99999999'"
+func isOpAndPunc(r rune) bool {
+	return strings.ContainsRune("+-*/%&|^<>=!:.([{,)]};", r)
+}
 
-	l := lexer.New(teste_1, Root)
+func main() {
+	f, _ := os.Open("teste.txt")
+	txt, _ := ioutil.ReadAll(f)
+	//fmt.Print(string(txt))
+
+	l := lexer.New(string(txt), Root)
 
 	l.Start()
 
-	for tok, done := l.NextToken(); !done; tok,done = l.NextToken() {
+	for tok, done := l.NextToken(); !done; tok, done = l.NextToken() {
 		fmt.Printf("%q - %q\n", tok.Value, tok.Type)
 	}
-	//for r := l.Next(); r != lexer.EOFRune ; {
-	//	fmt.Println(string(r))
-	//	r = l.Next()
-	//}
-
 }
 
 func Root(l *lexer.L) lexer.StateFunc {
@@ -72,28 +75,27 @@ func Root(l *lexer.L) lexer.StateFunc {
 		return CheckKeyword(l)
 	}
 
-
 	if r == '_' {
 		return Identifier(l)
-		} else if r == '\'' {
+	} else if r == '\'' {
 		return Runes(l)
-		} else if unicode.IsSymbol(r) || unicode.IsPunct(r) {
-			if r == '.' {
-				l.Next()
-				if unicode.IsDigit(l.Peek()) {
-					l.Rewind()
-					return Float(l)
-				}
+	} else if r == '"' {
+		return Strings(l)
+	} else if unicode.IsSymbol(r) || unicode.IsPunct(r) {
+		if r == '.' {
+			l.Next()
+			if unicode.IsDigit(l.Peek()) {
 				l.Rewind()
+				return Float(l)
 			}
-			return OpAndPunc(l)
-		} else if unicode.IsDigit(r) {
-			return Numbers(l)
-		} else if r == '.' {
-			return Float(l)
-		//} else if r == '"' {
-		//	return Strings(l)
-	} else if r != lexer.EOFRune {
+			l.Rewind()
+		}
+		return OpAndPunc(l)
+	} else if unicode.IsDigit(r) {
+		return Numbers(l)
+	} else if r == '.' {
+		return Float(l)
+	}  else if r != lexer.EOFRune {
 		l.Error(fmt.Sprintf("Símbolo desconhecido %q", r))
 	}
 
@@ -101,6 +103,7 @@ func Root(l *lexer.L) lexer.StateFunc {
 }
 
 func CheckKeyword(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou CK")
 	r := l.Next()
 	for unicode.IsLetter(r) {
 		r = l.Next()
@@ -113,15 +116,15 @@ func CheckKeyword(l *lexer.L) lexer.StateFunc {
 		}
 	}
 
-
 	return Identifier(l)
 }
 
 func Identifier(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou Id")
 	r := l.Next()
 
 	for unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' {
-		fmt.Println("ID: " + string(r))
+		//fmt.Println("ID: " + string(r))
 		r = l.Next()
 	}
 	l.Rewind()
@@ -131,22 +134,31 @@ func Identifier(l *lexer.L) lexer.StateFunc {
 }
 
 func OpAndPunc(l *lexer.L) lexer.StateFunc {
-	l.Take("+-*/%&|^<>=!:.([{,)]};")
-	for _, smb := range OpAndPuncs {
-		if l.Current() == smb {
-			l.Emit(OpAndPuncToken)
-			return Root(l)
-		}
+	var counter int
+	for isOpAndPunc(l.Next()) {
+		counter++
 	}
+	l.Rewind()
+	for ; counter > 0; counter-- {
+		for _, smb := range OpAndPuncs {
+			if l.Current() == smb {
+				l.Emit(OpAndPuncToken)
+				return Root(l)
+			}
+		}
+		l.Rewind()
+	}
+
 	l.Error(fmt.Sprintf("Operador/Pontuação desconhecido: %q", l.Current()))
 	return nil
 }
 
 func Numbers(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou Numb")
 	l.Take("0123456789")
 
 	r := l.Peek()
-	if unicode.IsSpace(r) || r == lexer.EOFRune {
+	if !unicode.IsLetter(r) || r == lexer.EOFRune {
 		l.Emit(IntLiteralToken)
 		return Root(l)
 	} else if r == '.' {
@@ -154,7 +166,7 @@ func Numbers(l *lexer.L) lexer.StateFunc {
 	} else if r == 'i' {
 		l.Next()
 		r = l.Peek()
-		if unicode.IsSpace(r) || r == lexer.EOFRune {
+		if !unicode.IsLetter(r) || r == lexer.EOFRune {
 			l.Emit(ImagLiteralToken)
 			return Root(l)
 		} else {
@@ -167,6 +179,7 @@ func Numbers(l *lexer.L) lexer.StateFunc {
 }
 
 func Float(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou float")
 	l.Next()
 	l.Take("0123456789")
 
@@ -202,11 +215,12 @@ func Float(l *lexer.L) lexer.StateFunc {
 }
 
 func Runes(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou Runes")
 	l.Next()
 	r := l.Next()
 	if r == '\\' {
 		r = l.Next()
-		if r == ('a'|'b'|'f'|'n'|'r'|'t'|'v'|'\\'|'\'') {
+		if strings.ContainsRune("abcfnrtv\\'", r) {
 			r = l.Next()
 			if r == '\'' {
 				l.Emit(RuneLiteralToken)
@@ -228,7 +242,7 @@ func Runes(l *lexer.L) lexer.StateFunc {
 			r = l.Next()
 			var cont int
 			ok := true
-			for ; ok && r != '\'';  {
+			for ok && r != '\'' {
 				ok = isHex(r)
 				r = l.Next()
 				cont++
@@ -237,7 +251,7 @@ func Runes(l *lexer.L) lexer.StateFunc {
 				l.Emit(RuneLiteralToken)
 				return Root(l)
 			}
-		}  else {
+		} else {
 			l.Error(fmt.Sprintf("Literal rune inválido: %q", l.Current()))
 		}
 	} else {
@@ -252,15 +266,71 @@ func Runes(l *lexer.L) lexer.StateFunc {
 	return nil
 }
 
-func String(l *lexer.L) lexer.StateFunc {
+func Strings(l *lexer.L) lexer.StateFunc {
+	fmt.Println("Entrou string")
 	r := l.Next()
 	if r == '`' {
-		for ok := true; ok ; r = l.Next() {
+		for ok := true; ok; r = l.Next() {
 			if r == '`' {
 				ok = false
+			} else if r == lexer.EOFRune {
+				l.Error(fmt.Sprintf("` Esperado."))
+				return nil
 			}
 		}
 		l.Emit(StringLiteralToken)
 		return Root(l)
+	} else {
+		r = l.Next()
+		for ; r != '"' && r != lexer.EOFRune; r = l.Next() {
+			for unicode.IsSpace(r) {
+				if r == '\n' {
+					l.Error(fmt.Sprintf("Quebra de linha inesperada: %q", l.Current()))
+					return nil
+				}
+				continue
+			}
+			if r == '\\' {
+				r = l.Next()
+				if strings.ContainsRune("abcfnrtv\\\"", r) {
+					continue
+				} else if isOctal(r) {
+					if isOctal(l.Next()) && isOctal(l.Next()) {
+						continue
+					}
+				} else if r == 'x' {
+					r = l.Next()
+					if isHex(r) && isHex(l.Next()) {
+						continue
+					}
+				} else if r == 'u' {
+					r = l.Next()
+					if isHex(r) && isHex(l.Next()) && isHex(l.Next()) {
+						continue
+					}
+				} else if r == 'U' {
+					r = l.Next()
+					var cont int
+					ok := true
+					for ok && !unicode.IsSpace(r) && r != '"' {
+						ok = isHex(r)
+						r = l.Next()
+						cont++
+					}
+					if ok && cont == 8 {
+						continue
+					}
+				} else {
+					l.Error(fmt.Sprintf("Literal string inválido: %q", l.Current()))
+					return nil
+				}
+			}
+		}
+		if r == lexer.EOFRune {
+			l.Error(fmt.Sprintf("\" Esperado."))
+			return nil
+		}
 	}
+	l.Emit(StringLiteralToken)
+	return Root(l)
 }
